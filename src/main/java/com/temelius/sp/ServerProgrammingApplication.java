1c4729d70f5fcd14ad4be130bcb3c2ecf7a5b537
Temelius <teme989@gmail.com>
2020-09-03 15:18:09 +0300

    Initial commit start.spring.io

---
+++ src/main/java/com/temelius/sp/ServerProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.temelius.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temelius/sp/ServerProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temelius.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
